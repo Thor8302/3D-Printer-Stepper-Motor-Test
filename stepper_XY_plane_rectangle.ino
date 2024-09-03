f821e14b764f11cc9bd9dd4a3b218322a00b5f8d
Thor8302 <68622476+Thor8302@users.noreply.github.com>
2024-09-04 03:21:29 +0530

    Add Stepper moter test code and demo video

---
+++ stepper_XY_plane_rectangle.ino
@@ -0,0 +1,28 @@
+
+
+#include <AFMotor.h>
+// Motor with 200 steps per rev (1.8 degree)
+// to motor port #1 (M1 and M2)
+AF_Stepper motorX(200, 2);
+AF_Stepper motorY(200, 1);
+void setup() {
+// set up Serial library at 9600 bps
+Serial.begin(9600);
+//Serial.println(“ Stepper test!”);  
+motorX.setSpeed(100); // 50 rpm
+motorY.setSpeed(100); // 50 rpm
+}
+void loop() {
+
+//Serial.println(“Micrsostep steps”);
+
+motorX.step(70, FORWARD, MICROSTEP);
+delay(10);
+motorY.step(70, FORWARD, MICROSTEP);
+delay(10);
+motorX.step(70, BACKWARD, MICROSTEP);
+delay(10);
+motorY.step(70, BACKWARD, MICROSTEP);
+
+delay(2000);
+}
---
+++ stepper_dvd_test.ino
@@ -0,0 +1,29 @@
+
+
+#include <AFMotor.h>
+// Motor with 200 steps per rev (1.8 degree)
+// to motor port #1 (M1 and M2)
+AF_Stepper motor(200, 2);
+void setup() {
+// set up Serial library at 9600 bps
+Serial.begin(9600);
+//Serial.println(“ Stepper test!”);  
+motor.setSpeed(100); // 50 rpm
+}
+void loop() {
+
+//Serial.println(“Micrsostep steps”);
+motor.step(50, FORWARD, MICROSTEP);
+motor.step(50, BACKWARD, MICROSTEP);
+motor.step(100, FORWARD, MICROSTEP);
+motor.step(100, BACKWARD, MICROSTEP);
+motor.step(200, FORWARD, MICROSTEP);
+motor.step(200, BACKWARD, MICROSTEP);
+motor.step(250, FORWARD, MICROSTEP);
+motor.step(250, BACKWARD, MICROSTEP);
+delay(100);
+
+motor.step(3, FORWARD, MICROSTEP);
+motor.step(3, BACKWARD, MICROSTEP);
+delay(1000);
+}
